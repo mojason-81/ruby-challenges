6cdc7052618cfe0219ea40af6ddfbe48a3f56f29
mojason <jason@mojason.com>
2015-04-29 12:28:02 -0500

    renamed blog files to correct names

--- blog.rb
+++ blog.rb
@@ -1,43 +1,94 @@
-class BlogPost
-		
-	def initialize(post_title, post_content)
-		@post_title = post_title
-		@post_content = post_content
-		@time_created = Time.now
+class Blog
+	attr_accessor :name
+	def initialize
+		puts "Name your blog."
+		@blog_name = gets.chomp
+		@@all_posts = []
 	end
 	
-	def get_post_title
-		return @post_title
+	def add_post
+		@@all_posts << BlogPost.new
 	end
 	
-	def get_post_content
-		return @post_content
+	def view_posts
+		@@all_posts.each{ |post|
+			puts
+			puts post.created
+			puts "Author: #{post.author}"
+			puts post.title
+			puts post.content
+		}
 	end
 	
-	def get_count
-		return @@counter
+	def sort_desc
+		@@all_posts.sort {|a, b|
+			b.created <=> a.created
+		}
+	end
+	
+	def sort_ascend
+		@@all_posts.sort {|a,b|
+			a.created <=> b.created
+		}
 	end
 	
 end
 
-posts_with_contents = Hash.new
-
-def add_to_hash(post_title, post_content, posts_with_contents, time_created)
-	posts_with_contents.store(post_title, post_content)
-	puts posts_with_contents
-	return posts_with_contents
+class BlogPost 
+	attr_reader :created
+	attr_reader :author
+	attr_reader :title
+	attr_reader :content
+	def initialize
+		@created = Time.now
+		puts "Your name, please."
+		@author = gets.chomp
+		puts "Title of your post, please."
+		@title = gets.chomp
+		puts "Put your content here."
+		@content = gets.chomp
+	end	
 end
 
-puts "Would you like to add a blog post? Y/n"
+#blog = Blog.new
+puts "Would you like to create a new blog? Y/n"
 answer = gets.chomp.downcase
 
-while answer == "y"
-	puts "Enter your post title."
-	title = gets.chomp
-	puts "Enter your post contents."
-	contents = gets.chomp
-	post = BlogPost.new(title, contents)
-	add_to_hash(title, contents, posts_with_contents, time_created)
-	puts "Would you like to add a blog post? Y/n"
-	answer = gets.chomp.downcase
+if answer == "y"
+	blog = Blog.new
+	while answer == "y"
+		post = blog.add_post
+		puts "Would you like to add a post? Y/n"
+		answer = gets.chomp.downcase
+	end
+end
+	
+puts "Would you like to add to an existing blog? Y/n"
+response = gets.chomp.downcase
+
+if response == "y"
+	while response =="y"
+		puts = "What's the name of the blog you would like to add to?"
+		blog_name = gets.chomp
+		post = blog_name.add_post
+		puts "Would you like to add another post? Y/n"
+		response = gets.chomp.downcase
+	end
+end
+
+
+puts "How would you like to sort your posts? Enter 'A' for ascending or 'D' for decending."
+sorting = gets.chomp.downcase
+
+if sorting == "a"
+	blog.sort_ascend
+elsif sorting == "d"
+	blog.sort_desc
+end
+
+puts "Would you like to view your posts? Y/n"
+response = gets.chomp.downcase
+
+if response == "y"
+	blog.view_posts
 end
